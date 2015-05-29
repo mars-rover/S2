432bbdd83cec310adad0d2a7d6a29e134761bf4c
lijunge <lijunge@lijungede.local>
2015-05-30 00:28:23 +0800

    Initial Commit

---
+++ S2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S2
+//
+//  Created by lijunge on 15/5/30.
+//  Copyright (c) 2015年 lijunge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
